2f1b9c72501e2b4073da897165b323487ec1ebee
sayama_ryunosuke@comture.com <sayama_ryunosuke@comture.com>
2025-06-19 15:12:03 +0900

    delete frontend

--- frontend/src/components/ButtonDownload.stories.tsx
+++
@@ -1,3 +0,0 @@
-import ButtonDownload from './ButtonDownload';
-
-export const Ideal = () => <ButtonDownload text={'DownloadText'} />;
--- frontend/src/features/reasoning/components/ReasoningCard.stories.tsx
+++
@@ -1,5 +0,0 @@
-import ReasoningCard from './ReasoningCard';
-
-export const Ideal = () => (
-  <ReasoningCard content="This is the content of the reasoning card." />
-);
