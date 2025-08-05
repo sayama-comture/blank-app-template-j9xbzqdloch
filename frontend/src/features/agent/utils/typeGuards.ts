b1be2cd808c8f01214b65d0a7c6a759d65ae636c
sayama_ryunosuke@comture.com <sayama_ryunosuke@comture.com>
2025-08-05 17:14:30 +0900

    delete

--- frontend/src/features/agent/utils/typeGuards.ts
+++
@@ -1,7 +0,0 @@
-import { AgentTool, InternetAgentTool, BedrockAgentTool } from '../types';
-
-export const isInternetTool = (tool: AgentTool): tool is InternetAgentTool =>
-  tool.toolType === 'internet';
-
-export const isBedrockAgentTool = (tool: AgentTool): tool is BedrockAgentTool =>
-  tool.toolType === 'bedrock_agent';
